a14ad21bcff956938f7161a034048fb53d10ccb1
Igor19766 <135424601+Igor19766@users.noreply.github.com>
2023-10-04 00:11:12 +1000

    ok well

--- test22/src/Main.java
+++ test22/src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
+
+
+
+    public void test(){
+
+    }
     public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
